6be2d2e65ba7a74681d8ee7e7a044a8e95126329
vimalprakash <vimalprakash332@gmail.com>
2023-08-16 17:54:43 +0530

    prototype

---
+++ src/App.js
@@ -0,0 +1,156 @@
+import { upload } from '@testing-library/user-event/dist/upload';
+import './App.css';
+import Gun from 'gun'
+import {useEffect, useRef, useState} from 'react'
+
+const gun = Gun({
+  peers: ['http:192.168.1.108:5050/gun'] // Put the relay node that you want here
+})
+var students = [
+  {
+      id: 1001,
+      first_name: "John",
+      last_name: "Doe",
+      age: 18,
+      verified: false
+  },
+  {
+      id: 1002,
+      first_name: "Jane",
+      last_name: "Smith",
+      age: 17,
+      verified: false
+  },
+  {
+      id: 1003,
+      first_name: "Michael",
+      last_name: "Johnson",
+      age: 16,
+      verified: false
+  },
+  {
+      id: 1004,
+      first_name: "Emily",
+      last_name: "Williams",
+      age: 18,
+      verified: false
+  },
+  {
+      id: 1005,
+      first_name: "Daniel",
+      last_name: "Brown",
+      age: 17,
+      verified: false
+  },
+  {
+      id: 1006,
+      first_name: "Olivia",
+      last_name: "Jones",
+      age: 16,
+      verified: false
+  },
+  {
+      id: 1007,
+      first_name: "William",
+      last_name: "Taylor",
+      age: 18,
+      verified: false
+  },
+  {
+      id: 1008,
+      first_name: "Sophia",
+      last_name: "Anderson",
+      age: 17,
+      verified: false
+  },
+  {
+      id: 1009,
+      first_name: "James",
+      last_name: "Martinez",
+      age: 16,
+      verified: false
+  },
+  {
+      id: 1010,
+      first_name: "Ava",
+      last_name: "Hernandez",
+      age: 18,
+      verified: false
+  },
+];
+
+
+function App() {
+  const [getdata,setdata]=useState({id : 1000})
+  const idinput= useRef()
+  function getfromgun()
+  {
+    gun.get(idinput.current.value).once((node) => { // Retrieve the text value on startup
+      console.log(node)
+      if(node === undefined) {
+        gun.get('text').put({text: "Write the text here"})
+      } else {
+        console.log("Found Node")
+        setdata(node)
+        setverified(false)
+        
+      //  setdata(node)
+      }
+    })
+  }
+  function verifyuser()
+  { 
+    
+    gun.get(getdata.id).once((node) => { // Retrieve the text value on startup
+      console.log(node)  
+      if (node.verified)
+        {
+          alert("user all ready verified")
+          setdata(node)
+        }
+        else 
+        {
+          gun.get(getdata.id).put({verified:true})
+          
+          setverified(true)
+        }
+    })
+  }
+  
+
+  const [verified,setverified]= useState(false)
+  
+  useEffect(() => {
+    
+    gun.get(getdata.id).on((node) => { // Is called whenever text is updated
+      setdata(node)
+    })
+  }, [getdata])
+
+
+  function uploaddata()
+  {
+    students.map((data)=>{
+      gun.get(data['id']).put(data)
+      return console.log(data['id'])
+    })
+  }
+  return (
+    <div className="App">
+      
+      <input placeholder="student id"  ref={idinput}/>
+      <button onClick={getfromgun}>get</button>
+      
+      {getdata != null ? <button onClick={verifyuser}>verify</button> : ""} <br/>
+      {getdata != null ? "name :" + getdata.first_name: ""} <br/>
+      {getdata != null ? "verified :" + getdata.verified: ""} 
+      {/* {verified ? 'verified': ''} */}
+      <br/>
+      <button onClick={uploaddata}>upload</button><br/>
+      {verified?"verified":""}
+    </div>
+    
+  );
+}
+
+export default App;
